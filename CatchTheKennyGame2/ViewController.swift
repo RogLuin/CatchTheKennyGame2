adb5fe97c82e2dcd4611d209a116109c02b1a268
Gorkem Dikbakan <gorkem_gd@hotmail.com>
2025-02-14 12:53:35 +0300

    Initial Commit

---
+++ CatchTheKennyGame2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchTheKennyGame2
+//
+//  Created by Görkem Dikbakan on 14.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
